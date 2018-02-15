3f731d5fe6ea114c0de84f19cf579291116fa74e
Eden Mesfun <emesfun@gmail.com>
2018-02-15 19:45:22 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,15 +2,6 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dance
-
-  attr_accessor :name
-
-  extend MetaDancing
-
-  def initialize(name)
-    @name = name
-  end
-
-
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
