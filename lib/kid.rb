2e18e0c755a00a49df0e1dcb9acf7b9caabae264
Eden Mesfun <emesfun@gmail.com>
2018-02-15 20:04:50 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,8 +1,11 @@
 class Kid
   include Dance
 
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+ 
   attr_accessor :name
-
+ 
   def initialize(name)
     @name = name
   end
